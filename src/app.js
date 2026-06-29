94ea875eeaf61dd5df30b220b397ffc3efb5e941
diego-kelly <kellydiego927@gmail.com>
2026-06-29 19:04:41 +0000

    generador de cartas aleatorio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
+  
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♥", "♦", "♣", "♠"];
+
+  const numeros = [
+    "A", "2", "3", "4", "5", "6", "7",
+    "8", "9", "10", "J", "Q", "K"
+  ];
+  const indicePalo = Math.floor(Math.random() * palos.length);
+  const palo = palos[indicePalo];
+
+  const indiceNumero = Math.floor(Math.random() * numeros.length);
+  const numero = numeros[indiceNumero];
+
+  console.log(palo);
+  console.log(numero);
+
+  document.querySelector("#iconoArriba").innerHTML = palo;
+  document.querySelector("#numero").innerHTML = numero;
+  document.querySelector("#iconoBajo").innerHTML = palo;
+
+  if (palo === "♥" || palo === "♦") {
+    document.querySelector(".card").style.color = "red";
+  } else{
+    document.querySelector(".card").style.color= "black";
+  }
 };
